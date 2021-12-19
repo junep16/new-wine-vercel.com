3da991caac910b75b586120a187abce3f59776b3
June Park <digitalonnet@digitalonnet-ui-MacBook-Pro.local>
2021-12-19 21:53:35 +0900

    add files

---
+++ src/pages/wine.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+//import 가 머지?
+const WinePage: NextPage = () => {
+    return (
+        <div>
+            <h1>Wine</h1>
+      </div>
+    )
+}
+
+export default WinePage;
+//export 가 머지?
