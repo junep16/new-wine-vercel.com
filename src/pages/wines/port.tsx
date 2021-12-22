f53d68da201731bf538ef31cd40917ab7b61c2cb
junep16 <jiyunpark0816@gmail.com>
2021-12-23 00:24:52 +0900

    ✨ added beer pages

--- src/pages/wines/port.tsx
+++ src/pages/wines/port.tsx
@@ -12,7 +12,7 @@ const PortWinePage: NextPage = () => {
 
   return (
     <div>
-      <h1>Wine</h1>
+      <h1>Port Wines</h1>
       <main>
         {data.map((wineData: Wine) => {
           return (
--- src/pages/wines/whites.tsx
+++ src/pages/wines/whites.tsx
@@ -1,9 +1,29 @@
 import type { NextPage } from "next";
+import { Error, Loading, WineCard } from "../../components";
+import { useWineData } from "../../hooks/useWineData";
+import { Wine } from "../../types/Wine";
+
 
 const WhitesWinePage: NextPage = () => {
+  const name = 'whites';
+  const { data, error } = useWineData(name)
+
+  if(error) return <Error />
+  if(!data) return <Loading />
+
   return (
     <div>
-      <h1>Wine</h1>
+      <h1>White Wines</h1>
+      <main>
+        {data.map((wineData: Wine) => {
+          return (
+            <WineCard 
+              key={`whites-wine-list-${wineData.id}`} 
+              wineData={wineData}
+            />
+          )
+        })}
+      </main>
     </div>
   )
 }
